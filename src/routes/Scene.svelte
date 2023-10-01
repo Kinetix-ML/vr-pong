c50f4ec66ecfa1400be4ba6ed0099504b2405dc1
Maxwell Stone <maxwell@kinetixml.com>
2023-10-01 16:51:52 -0400

    tried adding lighting

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -1,16 +1,35 @@
 <script>
 	import { T } from '@threlte/core';
+	import * as THREE from 'three';
 </script>
 
 <T.PerspectiveCamera
 	makeDefault
-	position={[10, 10, 10]}
+	position={[0, 5, 10]}
 	on:create={({ ref }) => {
-		ref.lookAt(0, 1, 0);
+		ref.lookAt(0, 0, 0);
 	}}
 />
 
-<T.Mesh>
-	<T.BoxGeometry />
+<T.SpotLight
+	position={[-40, 40, -15]}
+	castShadow
+	on:create={({ ref }) => {
+		ref.castShadow = true;
+		ref.shadow.mapSize = new THREE.Vector2(1024, 1024);
+		ref.shadow.camera.far = 130;
+		ref.shadow.camera.near = 40;
+	}}
+/>
+
+<T.DirectionalLight position={[0, 5, 5]} castShadow />
+<T.Mesh
+	castShadow
+	receiveShadow
+	on:create={({ ref }) => {
+		ref.lookAt(0, 1, 0);
+	}}
+>
+	<T.BoxGeometry args={[1, 1, 2]} />
 	<T.MeshBasicMaterial />
 </T.Mesh>
